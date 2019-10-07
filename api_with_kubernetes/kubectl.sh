c3a888123caa336291ab4946cf89d6a3aef6cfed
Sherry Ummen <sherry.ummen@outlook.com>
2019-10-07 17:37:46 +0300

    kubectl script

---
+++ api_with_kubernetes/kubectl.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+kubectl create -f apiwithkubernetesmongodb-service.yaml,apiwithkubernetesmongodb-deployment.yaml,apiwithkubernetes-claim0-persistentvolumeclaim.yaml,apiwithkubernetes-service.yaml,apiwithkubernetes-deployment.yaml
+
+
+
